91989fcfc2c8b1f984e4f54c6ff2cade8704c173
Luis-EncoraDev <luis.arguelles@encora.com>
2025-08-12 14:07:03 -0500

    Created ErrorResponseDTO. Modified ExceptionConfig for standardizing exceptions

--- src/main/java/com/InventoryManager/InventoryManager/config/ExceptionConfig.java
+++ src/main/java/com/InventoryManager/InventoryManager/config/ExceptionConfig.java
@@ -1,6 +1,8 @@
 package com.InventoryManager.InventoryManager.config;
 
+import com.InventoryManager.InventoryManager.dto.ErrorResponseDTO;
 import com.InventoryManager.InventoryManager.dto.ProductException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,9 +13,29 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ExceptionConfig extends ResponseEntityExceptionHandler {
-    @ExceptionHandler({ProductException.class})
-    protected ResponseEntity<ProductException> handleProductException(ProductException ex, WebRequest request) {
-        return new ResponseEntity<>(ex.getStatus());
+    @ExceptionHandler(ProductException.class)
+    protected ResponseEntity<ErrorResponseDTO> handleProductException(ProductException ex, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                LocalDateTime.now(),
+                ex.getStatus().value(),
+                ex.getStatus().getReasonPhrase(),
+                ex.getMessage(),
+                path
+        );
+        return new ResponseEntity<>(errorResponse, ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponseDTO> handleAllExceptions(Exception ex, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                path
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
-
